37bd0ccf046662b543a54f3601b976a39e5dad28
LucasAbsalao <lsa4@cin.ufpe.br>
2025-04-14 12:15:02 -0300

    fix: fixed convolution h2h

--- image_classification/resnet50_cifar10_training.py
+++ image_classification/resnet50_cifar10_training.py
@@ -5,7 +5,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 
-from resnet import Bottleneck, ResNet50, ResNet
+from neural_networks.resnet import Bottleneck, ResNet50, ResNet
 
 transform_train = transforms.Compose([
     transforms.RandomHorizontalFlip(),
@@ -29,18 +29,21 @@ testloader = torch.utils.data.DataLoader(test, batch_size=128,shuffle=False, num
 
 classes = ['plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
 
-net = ResNet50(10).to('cpu')
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+net = ResNet50(10).to(device)
 
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.SGD(net.parameters(), lr=0.1, momentum=0.9, weight_decay=0.0001)
 scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, factor = 0.1, patience=5)
 
-EPOCHS = 200
+EPOCHS = 10
 for epoch in range(EPOCHS):
     losses = []
     running_loss = 0
     for i, inp in enumerate(trainloader):
-        inputs, labels = inputs.to('cpu'), labels.to('cpu')
+        inputs, labels = inp
+        inputs, labels = inputs.to(device), labels.to(device)
         optimizer.zero_grad()
     
         outputs = net(inputs)
@@ -60,3 +63,17 @@ for epoch in range(EPOCHS):
     scheduler.step(avg_loss)
             
 print('Training Done')
+
+
+correct = 0
+total = 0
+with torch.no_grad():
+    for (images, labels) in testloader:
+        images, labels = images.to(device), labels.to(device)
+        outputs = net(images)
+
+        _, predicted = torch.max(outputs.data, 1)
+        total += labels.size(0)
+        correct += (predicted==labels).sum().item()
+
+print(f"Accuracy on {len(testloader)} test images: {100*(correct/total)}%")
--- image_classification/scratchCNN_cifar10_training.py
+++ image_classification/scratchCNN_cifar10_training.py
@@ -2,12 +2,12 @@ import torch
 import torch.nn as nn
 import torchvision
 import torchvision.transforms as transforms
-from scratch_cnn import CNN
+from neural_networks.scratch_cnn import CNN
 
 batch_size = 64
 num_classes = 10
 learning_rate = 0.001
-num_epochs = 20
+num_epochs = 5
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
@@ -64,3 +64,20 @@ for epoch in range(num_epochs):
         optimizer.step()
 
     print('Epoch [{}/{}], Loss: {:.4f}'.format(epoch+1, num_epochs, loss.item()))
+
+
+#TESTING
+with torch.no_grad():
+    correct = 0
+    total = 0
+    for images, labels in test_loader:
+        images = images.to(device)
+        labels = labels.to(device)
+        outputs = model(images)
+
+        _, predicted = torch.max(outputs.data, 1)
+
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+
+    print('Accuracy of the network on the {} train images: {} %'.format(len(test_loader), 100 * correct/total))
